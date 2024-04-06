3e0f47917be7e78f2ee604c3727cf56927c01234
Mindo21 <matusm44@gmail.com>
2024-04-06 23:10:25 +0200

    daco

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,52 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io"
+
+interface ServerToClientEvents {
+    noArg: () => void;
+    basicEmit: (a: number, b: string, c: Buffer) => void;
+    withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+    hello: () => void;
+}
+
+interface InterServerEvents {
+    ping: () => void;
+}
+
+interface SocketData {
+    name: string;
+    age: number;
+}
 
 dotenv.config();
 
 const app: Express = express();
+app.use(express.static('src/client/build'));
+app.use(cors());
+
+const server = http.createServer(app);
 const port = process.env.PORT || 3001;
 
-app.use(express.static('src/client/build'));
+const io = new Server<
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData
+>( server, {
+    cors: {
+        origin: `http://localhost:3000`
+    }
+});
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected: ', socket.id);
+});
+
+server.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
